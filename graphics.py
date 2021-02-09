999cf056318e5f92d93ed91388075b0fde6a9b23
IPPK93 <karimov.pd@phystech.edu>
2021-02-09 18:56:22 +0300

    Make build_floor work + change arguments in Player, Field

--- graphics.py
+++ graphics.py
@@ -1,53 +1,58 @@
 import pygame
 from maze_generation import *
 from player import *
+from colors import *
+from field import *
 
 
-MAZE_HEIGHT = 10
-MAZE_WIDTH = 30
+MAZE_H = 10
+MAZE_W = 30
 
 CELL_SIZE = 40
 PADDING = 10
 
 WALL_COLOR = (10, 10, 10)
 
-WINDOW_HEIGHT = (PADDING + CELL_SIZE) * MAZE_HEIGHT + PADDING  # +PADDING for upper border
-WINDOW_WIDTH = (PADDING + CELL_SIZE) * MAZE_WIDTH + PADDING    # +PADDING for left border
+WIN_H = (PADDING + CELL_SIZE) * MAZE_H + PADDING  # +PADDING for upper border
+WIN_W = (PADDING + CELL_SIZE) * MAZE_W + PADDING    # +PADDING for left border
 
+FLOOR_NUM = 3
              
 pygame.init()
-screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+screen = pygame.display.set_mode((WIN_W, WIN_H))
 screen.fill(Color.GREY.value)
 running = True
 
-field = Field(WINDOW_WIDTH, WINDOW_HEIGHT, CELL_SIZE, PADDING, MAZE_WIDTH, MAZE_HEIGHT)
+
+player = Player("semicolon_three.png", CELL_SIZE, PADDING, (PADDING, PADDING), (0, 0))
+field = Field((WIN_W, WIN_H), (CELL_SIZE, Color.PINK.value, PADDING, Color.BLACK.value), (MAZE_W, MAZE_H), FLOOR_NUM, player)
+field.build_floor((field.player.x_pos, field.player.y_pos))
+print("BUILT")
 field.draw_maze()
 
-player = Player("semicolon_three.png", CELL_SIZE, PADDING, PADDING, PADDING)
-p_start_x, p_start_y = 0, 0
 
 screen.blit(field.surface, (0, 0))
-screen.blit(player.surf, player.rect)
+screen.blit(field.player.surf, field.player.rect)
 pygame.display.flip()
 
 while running:
-    cur_cell = field.maze.grid[player.y_pos + p_start_y][player.x_pos + p_start_x]
+    cur_cell = field.get_maze().grid[player.y_pos][player.x_pos]
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_w or event.key == pygame.K_UP:
-                player.move(Direction.UP, cur_cell)
+                field.move_player(Direction.UP, cur_cell)
             elif event.key == pygame.K_s or event.key == pygame.K_DOWN:
-                player.move(Direction.DOWN, cur_cell)
+                field.move_player(Direction.DOWN, cur_cell)
             elif event.key == pygame.K_a or event.key == pygame.K_LEFT:
-                player.move(Direction.LEFT, cur_cell)
+                field.move_player(Direction.LEFT, cur_cell)
             elif event.key == pygame.K_d or event.key == pygame.K_RIGHT:
-                player.move(Direction.RIGHT, cur_cell)
+                field.move_player(Direction.RIGHT, cur_cell)
 
     screen.fill(Color.GREY.value)
     screen.blit(field.surface, (0, 0))
-    screen.blit(player.surf, player.rect)
+    screen.blit(field.player.surf, field.player.rect)
     pygame.display.flip()
 
 pygame.display.quit()
--- player.py
+++ player.py
@@ -1,44 +1,32 @@
 from maze_generation import *
 import pygame
 
+# field_pos = (field_x_pos, field_y_pos) - start position in field
+# maze_pos = (maze_x_pos, maze_y_pos) - start position in maze
+
 class Player(pygame.sprite.Sprite):
-    def __init__(self, image, size, padding, x_pos, y_pos):
+    def __init__(self, image, size, padding, field_pos, maze_pos):
         super(Player, self).__init__()
         self.surf = pygame.image.load(image).convert()
         self.surf = pygame.transform.scale(self.surf, (size, size))
         self.rect = self.surf.get_rect()
-        self.rect = self.rect.move(x_pos, y_pos)
+        self.rect = self.rect.move(*field_pos)
         self.step_len = size + padding
-        self.x_pos, self.y_pos = 0, 0
+        self.x_pos, self.y_pos = maze_pos
         
 
     def move(self, direction, cell):
         if direction in cell:
-            if (direction == Direction.UP):
+            if direction == Direction.UP:
                 self.rect = self.rect.move(0, -self.step_len)
                 self.y_pos -= 1
-            elif (direction == Direction.DOWN):
+            elif direction == Direction.DOWN:
                 self.rect = self.rect.move(0, self.step_len)
                 self.y_pos += 1
-            elif (direction == Direction.RIGHT):
+            elif direction == Direction.RIGHT:
                 self.rect = self.rect.move(self.step_len, 0)
                 self.x_pos += 1
             else:  # direction == Direction.LEFT
                 self.rect = self.rect.move(-self.step_len, 0)
                 self.x_pos -= 1
     
-
-
-# m = Maze(10, 10)
-# m.print_maze()
-# board = m.grid
-# p = Player(1, 1)
-# print()
-# print("player is at [{};{}]".format(p.x_pos, p.y_pos))
-# print("can we move from 1,1 to right? {}".format(Direction.RIGHT in m.grid[1][1]))
-# p.move(Direction.UP, m)
-# print("player is at [{};{}]".format(p.x_pos, p.y_pos))
-# p.move(Direction.LEFT, m)
-# print("player is at [{};{}]".format(p.x_pos, p.y_pos))
-# p.move(Direction.DOWN, m)
-# print("player is at [{};{}]".format(p.x_pos, p.y_pos))
